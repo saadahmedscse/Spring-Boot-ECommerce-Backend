cf505ff9564b401d56c42255aa1c8a8cd296de73
saadahmedscse <saadahmedscse@gmail.com>
2023-07-13 16:36:19 +0600

    added account creation functionality

---
+++ src/main/java/com/saadahmedev/ecommerce/service/auth/AuthServiceImpl.java
@@ -0,0 +1,36 @@
+package com.saadahmedev.ecommerce.service.auth;
+
+import com.saadahmedev.ecommerce.dto.auth.SignUpRequest;
+import com.saadahmedev.ecommerce.entity.User;
+import com.saadahmedev.ecommerce.repository.auth.AuthRepository;
+import com.saadahmedev.ecommerce.util.ValidatorUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    @Autowired
+    private AuthRepository authRepository;
+
+    @Override
+    public ResponseEntity<?> createAccount(SignUpRequest signUpRequest) {
+        ResponseEntity<?> validationResult = ValidatorUtil.isSignUpRequestValid(signUpRequest);
+
+        if (validationResult.getStatusCode().isSameCodeAs(HttpStatus.CREATED)) {
+            User user = User.builder()
+                    .firstName(signUpRequest.getFirstName())
+                    .lastName(signUpRequest.getLastName())
+                    .email(signUpRequest.getEmail())
+                    .phone(signUpRequest.getPhone())
+                    .profilePicture(signUpRequest.getProfilePicture())
+                    .password(signUpRequest.getPassword()).build();
+
+            authRepository.save(user);
+        }
+
+        return validationResult;
+    }
+}
