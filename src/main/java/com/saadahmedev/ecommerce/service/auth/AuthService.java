be2866b767f18e11d8f0482ab5ba7d0021424622
saadahmedscse <saadahmedscse@gmail.com>
2023-07-13 16:35:58 +0600

    created create account method

---
+++ src/main/java/com/saadahmedev/ecommerce/service/auth/AuthService.java
@@ -0,0 +1,11 @@
+package com.saadahmedev.ecommerce.service.auth;
+
+import com.saadahmedev.ecommerce.dto.auth.SignUpRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface AuthService {
+
+    ResponseEntity<?> createAccount(SignUpRequest signUpRequest);
+}
