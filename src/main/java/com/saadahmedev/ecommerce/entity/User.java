c691b2436bb3e655a92d7e45c8bfabdebd91e398
saadahmedscse <saadahmedscse@gmail.com>
2023-07-13 16:36:36 +0600

    created user entity

---
+++ src/main/java/com/saadahmedev/ecommerce/entity/User.java
@@ -0,0 +1,26 @@
+package com.saadahmedev.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_table")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private byte[] profilePicture;
+    private String phone;
+}
