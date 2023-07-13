c935b1fcaf6333dcdbe13613d63a1c0bc16feeb3
saadahmedscse <saadahmedscse@gmail.com>
2023-07-13 23:21:36 +0600

    added find by email method

--- src/main/java/com/saadahmedev/ecommerce/repository/auth/AuthRepository.java
+++ src/main/java/com/saadahmedev/ecommerce/repository/auth/AuthRepository.java
@@ -3,5 +3,9 @@ package com.saadahmedev.ecommerce.repository.auth;
 import com.saadahmedev.ecommerce.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AuthRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 }
