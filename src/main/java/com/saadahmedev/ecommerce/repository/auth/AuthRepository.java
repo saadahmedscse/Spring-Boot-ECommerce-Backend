b42b8f7cde94df6e2117e2819788348a37fd14cd
saadahmedscse <saadahmedscse@gmail.com>
2023-07-15 04:16:53 +0600

    added findFirstByEmail method

--- src/main/java/com/saadahmedev/ecommerce/repository/auth/AuthRepository.java
+++ src/main/java/com/saadahmedev/ecommerce/repository/auth/AuthRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface AuthRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
+    User findFirstByEmail(String email);
 }
