b9c8029b32d4363ac33b4bb49b8ed18b2bc248c4
saadahmedscse <saadahmedscse@gmail.com>
2023-07-15 04:17:03 +0600

    added login method

--- src/main/java/com/saadahmedev/ecommerce/service/auth/AuthService.java
+++ src/main/java/com/saadahmedev/ecommerce/service/auth/AuthService.java
@@ -1,5 +1,6 @@
 package com.saadahmedev.ecommerce.service.auth;
 
+import com.saadahmedev.ecommerce.dto.auth.LoginRequest;
 import com.saadahmedev.ecommerce.dto.auth.SignUpRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,6 @@ import org.springframework.stereotype.Service;
 public interface AuthService {
 
     ResponseEntity<?> createAccount(SignUpRequest signUpRequest);
+
+    ResponseEntity<?> login(LoginRequest loginRequest);
 }
