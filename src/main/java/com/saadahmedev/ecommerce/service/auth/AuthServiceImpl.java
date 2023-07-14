ecd6791bda04ea0a215adf05bb1871471338fe0e
saadahmedscse <saadahmedscse@gmail.com>
2023-07-15 04:17:28 +0600

    implemented login functionality

--- src/main/java/com/saadahmedev/ecommerce/service/auth/AuthServiceImpl.java
+++ src/main/java/com/saadahmedev/ecommerce/service/auth/AuthServiceImpl.java
@@ -1,12 +1,20 @@
 package com.saadahmedev.ecommerce.service.auth;
 
+import com.saadahmedev.ecommerce.dto.auth.LoginRequest;
+import com.saadahmedev.ecommerce.dto.auth.LoginResponse;
 import com.saadahmedev.ecommerce.dto.auth.SignUpRequest;
 import com.saadahmedev.ecommerce.entity.User;
 import com.saadahmedev.ecommerce.repository.auth.AuthRepository;
+import com.saadahmedev.ecommerce.service.jwt.UserDetailsServiceImpl;
+import com.saadahmedev.ecommerce.util.JwtUtil;
 import com.saadahmedev.ecommerce.util.ValidatorUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +25,20 @@ public class AuthServiceImpl implements AuthService {
 
     @Autowired
     private ValidatorUtil validatorUtil;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private String jwt = null;
 
     @Override
     public ResponseEntity<?> createAccount(SignUpRequest signUpRequest) {
@@ -29,9 +51,30 @@ public class AuthServiceImpl implements AuthService {
                     .email(signUpRequest.getEmail())
                     .phone(signUpRequest.getPhone())
                     .profilePicture(signUpRequest.getProfilePicture())
-                    .password(signUpRequest.getPassword()).build();
+                    .password(passwordEncoder.encode(signUpRequest.getPassword())).build();
 
             authRepository.save(user);
+        }
+
+        return validationResult;
+    }
+
+    @Override
+    public ResponseEntity<?> login(LoginRequest loginRequest) {
+        ResponseEntity<?> validationResult = validatorUtil.isLoginRequestValid(loginRequest);
+
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequest.getEmail());
+
+        jwt = jwtUtil.generateToken(userDetails);
+
+        if (validationResult.getStatusCode().isSameCodeAs(HttpStatus.OK)) {
+            validationResult = new ResponseEntity<>(new LoginResponse(
+                    true,
+                    "Logged in successfully",
+                    jwt
+            ), HttpStatus.OK);
         }
 
         return validationResult;
