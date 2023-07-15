29e2f02cf3d0d6e3e0a0687aacbad0fb34c41d1e
saadahmedscse <saadahmedscse@gmail.com>
2023-07-15 14:34:12 +0600

    created token data entity class

---
+++ src/main/java/com/saadahmedev/ecommerce/entity/TokenData.java
@@ -0,0 +1,21 @@
+package com.saadahmedev.ecommerce.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "token_table")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenData {
+    @Id
+    private String id;
+    private String token;
+}
