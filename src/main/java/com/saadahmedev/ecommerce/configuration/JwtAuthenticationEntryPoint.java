569c28874e3fa14b902e1334bbb28f3e478b754c
saadahmedscse <saadahmedscse@gmail.com>
2023-07-15 14:46:11 +0600

    added custom response for unauthorized requests

--- src/main/java/com/saadahmedev/ecommerce/configuration/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/saadahmedev/ecommerce/configuration/JwtAuthenticationEntryPoint.java
@@ -3,6 +3,8 @@ package com.saadahmedev.ecommerce.configuration;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -12,6 +14,19 @@ import java.io.IOException;
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getLocalizedMessage());
+        response.addHeader("WWW-Authenticate", "Basic realm=\"Realm\"");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+        response.getWriter().format("""
+      {
+         "status": %d,
+         "title": "%s",
+         "detail": "%s"
+      }
+      """,
+                HttpStatus.UNAUTHORIZED.value(),
+                HttpStatus.UNAUTHORIZED.name(),
+                authException.getMessage());
     }
 }
