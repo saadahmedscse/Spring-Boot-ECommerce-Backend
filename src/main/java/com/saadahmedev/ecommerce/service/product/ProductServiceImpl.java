caa54f4d6e9544105f7312b6b85c12d6b88f4271
saadahmedscse <saadahmedscse@gmail.com>
2023-08-22 00:50:18 +0600

    created logic to save multiple products

--- src/main/java/com/saadahmedev/ecommerce/service/product/ProductServiceImpl.java
+++ src/main/java/com/saadahmedev/ecommerce/service/product/ProductServiceImpl.java
@@ -3,14 +3,14 @@ package com.saadahmedev.ecommerce.service.product;
 import com.saadahmedev.ecommerce.dto.common.ApiResponse;
 import com.saadahmedev.ecommerce.dto.product.ProductRequest;
 import com.saadahmedev.ecommerce.entity.product.Product;
-import com.saadahmedev.ecommerce.entity.product.Rating;
 import com.saadahmedev.ecommerce.repository.product.ProductRepository;
-import com.saadahmedev.ecommerce.repository.product.RatingRepository;
-import com.saadahmedev.ecommerce.service.product.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -18,27 +18,47 @@ public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    @Autowired
-    RatingRepository ratingRepository;
-
     @Override
     public ResponseEntity<?> createProduct(ProductRequest productRequest) {
-        Rating rating = Rating.builder()
-                .rate(productRequest.getRating().getRate())
-                .count(productRequest.getRating().getCount())
-                .build();
-
         productRepository.save(
                 Product.builder()
                         .title(productRequest.getTitle())
-                        .category(productRequest.getCategory())
                         .description(productRequest.getDescription())
                         .price(productRequest.getPrice())
-                        .image(productRequest.getImage())
-                        .rating(ratingRepository.save(rating)).build()
+                        .discountPercentage(productRequest.getDiscountPercentage())
+                        .rating(productRequest.getRating())
+                        .stock(productRequest.getStock())
+                        .brand(productRequest.getBrand())
+                        .category(productRequest.getCategory())
+                        .thumbnail(productRequest.getThumbnail())
+                        .images(productRequest.getImages())
+                        .build()
         );
 
         return new ResponseEntity<>(new ApiResponse(true, "Product added successfully"), HttpStatus.CREATED);
+    }
+
+    @Override
+    public ResponseEntity<?> createMultipleProducts(List<ProductRequest> productRequests) {
+        List<Product> products = new ArrayList<>();
+        for (ProductRequest productRequest : productRequests) {
+            products.add(Product.builder()
+                    .title(productRequest.getTitle())
+                    .description(productRequest.getDescription())
+                    .price(productRequest.getPrice())
+                    .discountPercentage(productRequest.getDiscountPercentage())
+                    .rating(productRequest.getRating())
+                    .stock(productRequest.getStock())
+                    .brand(productRequest.getBrand())
+                    .category(productRequest.getCategory())
+                    .thumbnail(productRequest.getThumbnail())
+                    .images(productRequest.getImages())
+                    .build());
+        }
+
+        productRepository.saveAll(products);
+
+        return new ResponseEntity<>(new ApiResponse(true, "All products added successfully"), HttpStatus.CREATED);
     }
 
     @Override
