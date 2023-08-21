0b4866f0ce9afc7acfd794b8a2779247feb71784
saadahmedscse <saadahmedscse@gmail.com>
2023-08-22 00:50:04 +0600

    added new method to create multiple products

--- src/main/java/com/saadahmedev/ecommerce/service/product/ProductService.java
+++ src/main/java/com/saadahmedev/ecommerce/service/product/ProductService.java
@@ -4,10 +4,14 @@ import com.saadahmedev.ecommerce.dto.product.ProductRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface ProductService {
 
     ResponseEntity<?> createProduct(ProductRequest productRequest);
+
+    ResponseEntity<?> createMultipleProducts(List<ProductRequest> productRequests);
 
     ResponseEntity<?> getProducts();
 
