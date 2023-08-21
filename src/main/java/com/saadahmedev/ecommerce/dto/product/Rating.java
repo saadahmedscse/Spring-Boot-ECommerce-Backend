c5e7808b717a42ce810ffb1d7125b800e3f5d674
saadahmedscse <saadahmedscse@gmail.com>
2023-08-22 00:50:30 +0600

    deleted rating related classes

--- src/main/java/com/saadahmedev/ecommerce/dto/product/Rating.java
+++
@@ -1,9 +0,0 @@
-package com.saadahmedev.ecommerce.dto.product;
-
-import lombok.Data;
-
-@Data
-public class Rating {
-    private double rate;
-    private int count;
-}
--- src/main/java/com/saadahmedev/ecommerce/repository/product/RatingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.saadahmedev.ecommerce.repository.product;
-
-import com.saadahmedev.ecommerce.entity.product.Rating;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RatingRepository extends JpaRepository<Rating, Long> {
-}
